ff89df9249727b25469dc82c3707bd5ce06c5b73
dk\ivd <ivd@sitecore.net>
2017-05-22 15:19:17 +0800

    #104178: Resolve the issue

---
+++ src/Sitecore.Support.104178/AddMaster.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Sitecore.Support.Buckets.Pipelines.UI
+{
+    using System;
+    using Sitecore.Buckets.Managers;
+    using Sitecore.Data.Events;
+    using Sitecore.Data.Items;
+    using Sitecore.Diagnostics;
+
+    [Serializable]
+    public class AddMaster : Shell.Framework.Commands.AddMaster
+    {
+        public AddMaster()
+        {
+            this.ItemCreated += OnItemCreated;
+        }
+
+        private void OnItemCreated([NotNull] object sender, [NotNull] ItemCreatedEventArgs args)
+        {
+            Assert.ArgumentNotNull(sender, "sender");
+            Assert.ArgumentNotNull(args, "args");
+
+            Item item = args.Item;
+            if (item != null && BucketManager.IsItemContainedWithinBucket(item))
+            {
+                Context.ClientPage.ClientResponse.Eval("setTimeout(function(){ scForm.invoke(\"item:load(id=" + item.ID + ")\"); }, 0)");
+            }
+        }
+    }
+}
---
+++ src/Sitecore.Support.104178/SelectItem.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace Sitecore.Support.Buckets.Pipelines.UI.AddFromTemplate
+{
+    using Sitecore.Buckets.Managers;
+    using Sitecore.Data.Items;
+    using Sitecore.Diagnostics;
+    using Sitecore.Web.UI.Sheer;
+
+    public class SelectItem
+    {
+        public void Execute([NotNull] ClientPipelineArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            if (!args.HasResult)
+            {
+                return;
+            }
+
+            Item item = (Context.ContentDatabase ?? Context.Database).GetItem(args.Result);
+            if (item != null && BucketManager.IsItemContainedWithinBucket(item))
+            {
+                Context.ClientPage.ClientResponse.Eval("setTimeout(function(){ scForm.invoke(\"item:load(id=" + item.ID + ")\"); }, 0)");
+            }
+        }
+    }
+}
